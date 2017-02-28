f226957848dd6f0e2185b5a6251f94e5d508a07d
Cyril <cyril-ix@live.fr>
2017-02-28 13:48:16 +0100

    origin

---
+++ view/template/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Data_Project</title>
+    </head>
+    <body>
+        <header>
+            <p>test1</p>
+        </header>
+        <main>
+            <?php include($page) ?>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
