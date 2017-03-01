bdd9d42fdf3e856d762c1f7a95a3d5adf50e72e9
Unknown <eddyroy2125@gmail.com>
2017-03-01 16:12:13 +0100

    Update index with popup.Update get methods

--- controller/get_methods.php
+++ controller/get_methods.php
@@ -1,18 +1,48 @@
 <?php
 
-   if (isset($_GET)) {
+
+		if($_GET['mass'] === 'heaviest'){
+			
+			$result = $planete->getHeaviestPlanete();
+		}
+		if($_GET['mass'] === 'lightest'){
+			
+			$result = $planete->getLightestPlanete();
+		}
+		if($_GET['temperature'] === 'coldest'){
+			
+			$result = $planete->getColdestPlanete();
+		}
+		if($_GET['temperature'] === 'hottest'){
+			
+			$result = $planete->getHottestPlanete();
+		}
+		if($_GET['year'] === 'oldest'){
+			
+			$result = $planete->getOldestPlanete();
+		}
+		if($_GET['year'] === 'youngest'){
+			
+			$result = $planete->getYoungestPlanete();
+		}
+		if($_GET['method'] === 'imaging'){
+			
+			$result = $planete->getPlaneteByImage();
+		}
+		if($_GET['method'] === 'transit'){
+			
+			$result = $planete->getPlaneteByTransit();
+		}
+		if($_GET['method'] === 'radial'){
+			
+			$result = $planete->getPlaneteByRadial();
+		}
+
+		
+		
+		   
 
 
-	   $result = $planete->getRandomPlanete();
 
-	   foreach ($result as $row) {
+			
 
-		print "<img src=''>";
-		print "<p>".$row->nom."</p>";
-		print "<p>".$row->discovered."</p>";
-		print "<p>".$row->detection_type."</p>";
-		print "<p>Système stellaire : ".$row->star_name."</p>";
-
-
-	   }
-   }
