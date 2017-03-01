85bb92b16ed44cbcdd0b6e7bea22a14b8a29ab4b
Unknown <cyril-ix@live.fr>
2017-03-01 11:47:41 +0100

    Update Class Planete

--- controller/class/Planete.php
+++ controller/class/Planete.php
@@ -4,7 +4,28 @@ class Planete{
 
     public function getRandomPlanete(){
         global $dbh;
+        $query = $dbh->query('SELECT * FROM planete ORDER BY RAND() LIMIT 1 ');
+        return $query->fetchAll();
+    }
 
+    public function getNextPlanete(){
+        global $dbh;
+        $idPlanete = $_GET['id_planete'];
+        $query = $dbh->prepare('SELECT * FROM planete WHERE id = :id_planete ');
+        $query = execute(array(
+            "id_planete" => $idPlanete+1 ;
+        ));
+        return $query->fetchAll();
+    }
+
+    public function getPrevPlanete(){
+        global $dbh;
+        $idPlanete = $_GET['id_planete'];
+        $query = $dbh->prepare('SELECT * FROM planete WHERE id = :id_planete');
+        $query = execute(array(
+            "id_planete" => $idPlanete-1 ;
+        ));
+        return $query->fecthAll();
     }
 
 
