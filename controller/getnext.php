bc4a1468b5e8bc8f47b083200e16c638bde213e9
Unknown <cyril-ix@live.fr>
2017-03-17 15:24:12 +0100

    Update select method. SelectMethod Ok

--- controller/getnext.php
+++ controller/getnext.php
@@ -1,18 +1,74 @@
 <?php
 require_once ('pdo.php');
 require_once ('class/Planete.php');
-$idplanete = $_POST['idplanete'];
 
 $planete = new Planete();
 
-$result = $planete->getNextPlanete($idplanete);
 
-$name = $result->star_name;
+if($_POST['methodee'] == 1){
 
-$res = $planete->getSameStarPlanete($name);
-$resimage = $planete->getImagePlanete($name);
+	$idplanete = $_POST['idplanete'];
 
-$tableau = array( 	"planete" => $result, 
-					"systeme" => $res,
-					"picture" => $resimage);
-echo json_encode($tableau);
+
+
+	$result = $planete->getNextPlanete($idplanete);
+
+	$name = $result->star_name;
+
+	$res = $planete->getSameStarPlanete($name);
+	$resimage = $planete->getImagePlanete($name);
+
+	$tableau = array( 	"planete" => $result,
+						"systeme" => $res,
+						"picture" => $resimage);
+	echo json_encode($tableau);
+}else{
+	$methode = "";
+    $select = "";
+    $count = $_POST['count'];
+    if($_POST['select'] != ""){
+        if($_POST['select'] == "poidAsc" ){
+            $select = " ORDER BY mass ASC ";
+        }else if($_POST['select'] == "poidDesc"){
+            $select = " ORDER BY mass DESC ";
+        }else if($_POST['select'] == "tempAsc"){
+            if($_POST['methode'] != ""){
+                $select = " AND temp_calculated != '0' OR temp_measured != '0' ORDER BY temp_calculated ASC ";
+            }else{
+                $select = " WHERE temp_calculated != '0' OR temp_measured != '0' ORDER BY temp_calculated ASC ";
+            }
+        }else if($_POST['select'] == "tempDesc"){
+            if($_POST['methode']!= ""){
+                $select = " AND temp_calculated != '0' OR temp_measured != '0' ORDER BY temp_calculated DESC";
+            }else{
+                $select = " WHERE temp_calculated != '0' OR temp_measured != '0' ORDER BY temp_calculated DESC ";
+            }
+        }else if($_POST['select'] == "anneeAsc"){
+            $select = " ORDER BY discovered ASC ";
+        }else if($_POST['select'] == "anneeDesc"){
+            $select = " ORDER BY discovered ASC ";
+        }
+    }
+
+    if($_POST['methode'] != ""){
+        if($_POST['methode'] == "Primary" ){
+            $methode = " WHERE detection_type = 'Primary Transit' ";
+        }else if($_POST['methode'] == "Radial"){
+            $methode = " WHERE detection_type = 'Radial Velocity' ";
+        }else if($_POST['methode'] == "Imaging"){
+            $methode = " WHERE detection_type = 'Imaging' ";
+        }
+    }
+
+    $result = $planete->getSpecialPlanetebySelect($methode,$select,$count);
+    $name = $result->star_name;
+    $res = $planete->getSameStarPlanete($name);
+	$resimage = $planete->getImagePlanete($name);
+
+    $array = array(
+        "planete" => $result,
+        "systeme" => $res,
+		"picture" => $resimage
+    );
+    echo json_encode($array);
+}
